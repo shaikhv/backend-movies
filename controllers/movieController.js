e8b0a22db11d784facb15ea4316d80488f3e17a4
shaikh <musavajahat@gmail.com>
2025-09-12 23:34:13 +0400

    Movie detail api

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -21,3 +21,26 @@ exports.createMultiMovie = async (req, res) => {
     res.status(500).json({ error: 'Error fetching movies' });
   }
 }
+
+exports.createMultiMovie = async (req, res) => {
+  try {
+    const movies = await NextMovie.insertMany(req.body);
+    console.log('Movies fetched:', movies);
+    res.status(200).json({ movies });
+  } catch (error) {
+    console.log('Error fetching movies:', error);
+    res.status(500).json({ error: 'Error fetching movies' });
+  }
+}
+
+exports.getMovieById = async (req, res) => {
+  console.log('Fetching movie with ID:', req.params.id);
+  try {
+    const movie = await NextMovie.findById(req.params.id);
+    console.log('Movie fetched:', movie);
+    res.status(200).json({ movie });
+  } catch (error) {
+    console.log('Error fetching movies:', error);
+    res.status(500).json({ error: 'Error fetching movies' });
+  }
+}
--- routes/movies.js
+++ routes/movies.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMovies, createMultiMovie } = require('../controllers/movieController');
+const { getAllMovies, createMultiMovie, getMovieById } = require('../controllers/movieController');
 
 router.get('/movies', getAllMovies);
 router.post('/createMultiMovies', createMultiMovie);
+router.get('/movie/:id', getMovieById);
 
 module.exports = router;
